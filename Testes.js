ec13c70782265ff43d59a447c2eedf009e3a3eb3
SirMonkii <sinniknot@gmail.com>
2022-06-13 14:46:35 -0300

    parabains

--- Testes.js
+++ Testes.js
@@ -11,9 +11,11 @@ rl.question("Qnts kgs tu pesa?", function (Kilos){
   {console.log();}
   if (Kilos >= 70){
     console.log("Vc tem um peso ok");
+    console.log("https://www.youtube.com/watch?v=1Mcdh2Vf2Xk&t=6s");
   } 
   else{
     console.log("Vc tem um peso abaixo da média");
+    console.log("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMRC3ZpprJQJ6tovvB3h9cO1ihFJSYtpP1_zln3anZeQbN1ml7-PdFVzvP-PaejDa9ya8&usqp=CAU");
   }
   {console.log();}
   rl.close();
